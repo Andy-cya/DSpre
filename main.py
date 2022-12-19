1d307caaf425869da90494f723d64cd92e102d5d
DataScience-final <120306358+DataScience-final@users.noreply.github.com>
2022-12-19 21:48:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -203,7 +203,10 @@ a = st.slider("隔墙数量：", 0, 2)
 b = st.select_slider('起止点相对关系：', options=['相邻墙上', '相对墙上'])
 if st.button("查询演示"):
     if a == 0 and b == '相邻墙上':
-        st.markdown("![[15.00,15.00,5.00,0,0,1].gif](https://i.postimg.cc/Hn5j8m3Z/15-00-15-00-5-00-0-0-1.gif)")
+        st.image(
+            "https://i.postimg.cc/Hn5j8m3Z/15-00-15-00-5-00-0-0-1.gif",
+        width=400,
+        )
         st.text('栅格大小为0.05m')
         st.markdown("![[15.00,15.00,5.00,0,0,2].gif](https://i.postimg.cc/dknGxYSf/15-00-15-00-5-00-0-0-2.gif)")
         st.text('栅格大小为0.05m')
