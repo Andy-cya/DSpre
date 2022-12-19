b8832cdc716f27255479b0fd1a56dc71e0fe429b
madara-2001 <80236601+madara-2001@users.noreply.github.com>
2022-12-19 20:10:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -203,9 +203,10 @@ a = st.slider("隔墙数量：", 0, 2)
 b = st.select_slider('起止点相对关系：', options=['相邻墙上', '相对墙上'])
 if st.button("查询演示"):
     if a == 0 and b == '相邻墙上':
-        st.markdown("![Alt Text](https://github.com/DataScience-final/automated-pipe-layout/blob/main/%5B15.00%2C%2015.00%2C%205.00%2C%200%2C%200%2C%201%5D.gif)")
+        image1 = Image.open('./[15.00, 15.00, 5.00, 0, 0, 1].gif')
         image2 = Image.open('./[15.00, 15.00, 5.00, 0, 0, 2].gif')
         image3 = Image.open('./[15.00, 15.00, 5.00, 0, 0, 3].gif')
+        st.image(image1)
         st.text('栅格大小为0.05m')
         st.image(image2)
         st.text('栅格大小为0.05m')
